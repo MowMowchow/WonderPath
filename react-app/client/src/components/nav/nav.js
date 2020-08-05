16bfe3fa7e22e4b1e29cd24a20dd071562e18e02
Jason Hou <darlstar.hou2@gmail.com>
2020-08-05 03:33:00 -0400

    inital commit

--- react-app/client/src/components/nav/nav.js
+++ react-app/client/src/components/nav/nav.js
@@ -1,24 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './nav.css';
 import { Link } from 'react-router-dom';
 
 class Nav extends Component {
 	render() {
 		return (
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </div>
-  </div>
-</nav>
+			<nav className="navbar navbar-expand-lg">
+				<a className="navbar-brand" href="#">
+					WonderPath
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon">o/c</span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<ul className="navbar-nav">
+						<Link to="/findpath">
+							<a className="nav-link">
+								<li className="nav-item">Find Path</li>
+							</a>
+						</Link>
+						<Link to="/about">
+							<a className="nav-link">
+								<li className="nav-item">About</li>
+							</a>
+						</Link>
+						<Link to="/signup">
+							<a className="nav-link">
+								<li className="nav-item">Sign Up</li>
+							</a>
+						</Link>
+					</ul>
+				</div>
+			</nav>
 		);
 	}
 }
