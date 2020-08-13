6d36f7d11fb00ca1df5b2243b26254450274c50c
Jason Hou <darlstar.hou2@gmail.com>
2020-08-13 01:08:18 -0400

    initial commit

--- react-app/client/src/components/home/home.js
+++ react-app/client/src/components/home/home.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import './home.css';
-import mocklogo from './mocklogo.png';
+import '../../wonderpath_icon/style.css';
 var ReactRotatingText = require('react-rotating-text');
 
 class Home extends Component {
 	render() {
 		return (
 			<div className="container-fluid ">
-				<div id="particles-js"> 
+				<div id="particles-js">
 					<div className="hero-container">
 						<div className="row logo-row">
 							<div className="col-lg-5 logo-col">
-								<img src={mocklogo} alt="lul" className="logo-pic" />
+								<span className="icon-wonderpath_logo_map-1 logo" />
 							</div>
 							<div className="col-lg-7 slogan-col">
 								<div className="row slogan-row-top">
 									<div className="col-12 slogan-col-top">
 										<h1 className="slogan-top-text">
 											Start
-											<ReactRotatingText items={[' Walking', ' Biking', ' Running', ' Moving']} />
+											<ReactRotatingText
+												items={[ ' Walking', ' Biking', ' Running', ' Moving' ]}
+											/>
 										</h1>
 									</div>
 								</div>
