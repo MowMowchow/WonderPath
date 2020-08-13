94af2a04563a6073472236c1c236055641bab6b3
Jason Hou <darlstar.hou2@gmail.com>
2020-08-13 01:10:50 -0400

    initial commit

--- react-app/client/src/components/nav/nav.js
+++ react-app/client/src/components/nav/nav.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './nav.css';
 import { Link } from 'react-router-dom';
-const base_url = 'http://localhost:3000'
+import '../../wonderpath_icon/style.css';
+
+const base_url = 'https://wonderpath.herokuapp.com'
 class Nav extends Component {
 	constructor(props) {
 		super(props);
